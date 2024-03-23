450525247dd540784b24c02abaa25e19ceae51dc
rogerchang1108 <rogerchang1108@gmail.com>
2024-03-23 15:10:56 +0800

    chore: remove unnecessary import of 'package:flutter/widgets.dart'

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 import 'package:flutter_app/widgets/information_page.dart';
 import 'package:flutter_app/widgets/new_expense.dart';
